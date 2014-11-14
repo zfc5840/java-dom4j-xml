384862f1f599cd1d3115d1f6e1b6153319697f37
zfc5840 <1076231986@qq.com>
2014-11-14 15:05:19 +0800

    修改子节点下套用子节点的特例

--- src/com/dom4j/xml/Dom4jXmlToList.java
+++ src/com/dom4j/xml/Dom4jXmlToList.java
@@ -19,6 +19,7 @@ public class Dom4jXmlToList {
 	//使用dom4j方式解析xml成String
 	public static  void xmlToList(String filename){
 		List<Map<String, String>> dbConnections = new ArrayList<Map<String, String>>();
+		//构建读取实例
 	     SAXReader saxReader = new SAXReader();
 	     Document document = null;
 		try {
@@ -26,25 +27,55 @@ public class Dom4jXmlToList {
 		} catch (DocumentException e) {
 			e.printStackTrace();
 		}
-	     Element connections = document.getRootElement();//获取根节点
+	     Element connections = document.getRootElement();//获取根节点，即bookstore
 		@SuppressWarnings("unchecked")
-		Iterator<Element> rootIter = connections.elementIterator();
+		Iterator<Element> rootIter = connections.elementIterator();//获取节点集合，即book集合
+		//循环遍历节点book
 	     while (rootIter.hasNext()) {
+	    	 //获取要遍历的节点，book节点
 	         Element connection = rootIter.next();
 			@SuppressWarnings("unchecked")
+			//获取book节点下子节点的集合
 			Iterator<Element> childIter = connection.elementIterator();
 			Map<String, String> connectionInfo = new HashMap<String, String>();
 	         @SuppressWarnings("unchecked")
+	         //获取book节点属性集合
 			List<Attribute> attributes = connection.attributes();
 	         for (int i = 0; i < attributes.size(); ++i) { // 添加节点属性
 	             connectionInfo.put(attributes.get(i).getName(), attributes.get(i).getValue());
 	         }
+	         //循环遍历book下子节点
 	        while (childIter.hasNext()) { // 添加子节点
+	        	//获取当前子节点
 	             Element attr = childIter.next();
+	             //判断当前节点是否存在子节点
+	             Iterator<Element> childattr=  attr.elementIterator();
+	             if(childattr.hasNext()){
+	            	 foreachElement(attr);
+	             }else{
+	            	 System.out.println("没有节点");
+	             }
 	             connectionInfo.put(attr.getName().trim(), attr.getText().trim());
 	         }
 	         dbConnections.add(connectionInfo);
 	     }
-	 System.out.println("ddd");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void foreachElement(Element element){
+		 Iterator<Element> childattr=  element.elementIterator();
+		 boolean bl=childattr.hasNext();
+         if(bl){
+        	 Element attr = childattr.next();
+        	 foreachElement(attr);
+        	 Iterator<Element> childattr2=  attr.elementIterator();
+    		 boolean bl2=childattr2.hasNext();
+    		 if(bl2){
+    			 System.out.println("节点名22："+attr.getName().trim()+"   节点值22："+ attr.getText().trim());
+    		 }
+        	
+         }else{
+        	 System.out.println("节点名："+element.getName().trim()+"   节点值："+ element.getText().trim());
+         }
 	}
 }
